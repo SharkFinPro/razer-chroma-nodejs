520478b1f6e2fe4f84785dfb997641f5669232dd
SharkFin <sharkfinpro@hotmail.com>
2019-12-28 16:22:43 -0800

    updated example
    added all objects to example.js

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 const Chroma = require("./chroma.js"),
     MousepadEffects = require("./mousepadEffects.js"),
     MouseEffects = require("./mouseEffects.js"),
-    HeadsetEffects = require("./headsetEffects.js");
+    HeadsetEffects = require("./headsetEffects.js"),
+    KeyboardEffects = require("./keyboardEffects.js"),
+    KeypadEffects = require("./keypadEffects.js"),
+    ChromalinkEffects = require("./chromalinkEffects.js");
 
 // Initialize Chroma
 Chroma.init(() => {
@@ -16,11 +19,23 @@ Chroma.init(() => {
     // Set the headset to blue
     const headsetEffect = HeadsetEffects.setColor(0xFF0000);
 
+    // Set the keyboard to red
+    const keyboardEffect = KeyboardEffects.setColor(0x0000FF);
+
+    // Set the keypad to yellow
+    const keypadEffect = KeypadEffects.setColor(0x00FFFF);
+
+    // Set the chromalink device to white
+    const chromalinkEffect = ChromalinkEffects.setColor(0xFFFFFF);
+
     // Clear all effects and close Chroma after 5 seconds
     setTimeout(() => {
         MousepadEffects.clear(mousepadEffect);
         MouseEffects.clear(mouseEffect);
         HeadsetEffects.clear(headsetEffect);
+        KeyboardEffects.clear(keyboardEffect);
+        KeypadEffects.clear(keypadEffect);
+        ChromalinkEffects.clear(chromalinkEffect);
         Chroma.uninit(() => {
             console.log("Chroma Editing Stopped");
         });
